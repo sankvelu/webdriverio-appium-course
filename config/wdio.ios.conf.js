ab93b7fcb0595276bad20bf8da8e6493f5ad10b6
sankaravelayutham <sankaravelayutham@yahoo.com>
2024-11-12 18:08:04 +0530

    Modified config

--- config/wdio.ios.conf.js
+++ config/wdio.ios.conf.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const { config } = require('./wdio.shared.conf');
+const path = require("path");
+const { config } = require("./wdio.shared.conf");
 
 // ====================
 // Runner Configuration
@@ -11,9 +11,7 @@ config.port = 4723;
 // ============
 // Specs
 // ============
-config.specs = [
-  path.join(process.cwd(), './test/specs/ios/webview*.js')
-];
+config.specs = [path.join(process.cwd(), "./test/specs/ios-findElements.js")];
 
 //
 // ============
@@ -25,22 +23,27 @@ config.capabilities = [
     "appium:platformVersion": "16.0",
     "appium:deviceName": "iPhone 14 Pro",
     "appium:automationName": "XCUITest",
-    "appium:app": path.join(process.cwd(), "app/ios/wdioNativeDemoApp.app"),
-  }
-]
+    "appium:app": path.join(process.cwd(), "app/ios/UIKitCatalog.app"),
+  },
+];
 
 //
 // Test runner services
 // Services take over a specific job you don't want to take care of. They enhance
 // your test setup with almost no effort. Unlike plugins, they don't add new
 // commands. Instead, they hook themselves up into the test process.
-config.services = [['appium', {
-  args: {
-    address: 'localhost',
-    port: 4723,
-    relaxedSecurity: true
-  },
-  logPath: './'
-}]];
+config.services = [
+  [
+    "appium",
+    {
+      args: {
+        address: "localhost",
+        port: 4723,
+        relaxedSecurity: true,
+      },
+      logPath: "./",
+    },
+  ],
+];
 
 exports.config = config;
