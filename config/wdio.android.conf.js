60f07de3220a565a2af4c3cd7707fca89c44f8f9
sankaravelayutham <sankaravelayutham@yahoo.com>
2024-11-14 11:14:26 +0530

    Text change1

--- config/wdio.android.conf.js
+++ config/wdio.android.conf.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const { config } = require('./wdio.shared.conf');
+const path = require("path");
+const { config } = require("./wdio.shared.conf");
 
 // ====================
 // Runner Configuration
@@ -12,7 +12,7 @@ config.port = 4723;
 // Specs
 // ============
 config.specs = [
-  path.join(process.cwd(), './test/specs/android/webview*.js')
+  path.join(process.cwd(), "./test/specs/android/delete-note.spec.js"),
 ];
 
 //
@@ -22,26 +22,34 @@ config.specs = [
 config.capabilities = [
   {
     platformName: "Android",
-    "appium:platformVersion": "10.0",
-    "appium:deviceName": "Nexus 6p",
+    "appium:platformVersion": "11.0",
+    "appium:deviceName": "Pixel 6",
     "appium:automationName": "UIAutomator2",
-    "appium:app": path.join(process.cwd(), "./app/android/ColorNote Notepad.apk"),
-    "appium:autoGrantPermissions": true
-  }
-]
+    "appium:app": path.join(
+      process.cwd(),
+      "./app/android/ColorNote Notepad.apk"
+    ),
+    "appium:autoGrantPermissions": true,
+  },
+];
 
 //
 // Test runner services
 // Services take over a specific job you don't want to take care of. They enhance
 // your test setup with almost no effort. Unlike plugins, they don't add new
 // commands. Instead, they hook themselves up into the test process.
-config.services = [['appium', {
-  args: {
-    address: 'localhost',
-    port: 4723,
-    relaxedSecurity: true
-  },
-  logPath: './'
-}]];
+config.services = [
+  [
+    "appium",
+    {
+      args: {
+        address: "localhost",
+        port: 4723,
+        relaxedSecurity: true,
+      },
+      logPath: "./",
+    },
+  ],
+];
 
 exports.config = config;
